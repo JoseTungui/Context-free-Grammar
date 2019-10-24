739ac24db4d30a92eb17921716dd250ad8456710
JoseTungui <a310996@uach.mx>
2019-10-23 19:11:32 -0600

    Update Grammar.py
    Example of finite automata

--- Grammar.py
+++ Grammar.py
@@ -1,3 +1,14 @@
-string = input("Enter your string: ")
+# Example of finite automata
 
-print("Tokens: ", string)
+dfa = {0:{'0':0, '1':1},
+       1:{'0':2, '1':0},
+       2:{'0':1, '1':2}}
+
+def accepts(transitions,initial,accepting,s):
+    state = initial
+    for c in s:
+        state = transitions[state][c]
+    return state in accepting
+
+print(accepts(dfa,0,{0},'1011101'))
+print(accepts(dfa,0,{0},'10111011'))
